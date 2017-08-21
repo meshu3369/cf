258df79fbac11498efd5cfaa08ab6e9c222d0db6
meshu3369 <meshu.uiu@gmail.com>
2017-08-21 14:14:14 +0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { PostComponent } from './post/post.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    PostComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule,
+      HttpModule,
+      RouterModule.forRoot([
+          {
+            path: 'details',
+            component: DetailsComponent
+          }
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
